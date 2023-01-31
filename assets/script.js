157e00ba4391f80f6796074c0cbf3f65f02db9e2
Saarah Khan <saarah_96@hotmail.co.uk>
2023-01-31 16:21:12 +0000

    1. updadted js file with current time and created columns 2. deleted code from html to create table on js file instead

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,51 @@
-var liveTime = $('#currentDay')
+var liveTime = $('#currentDay');
 
-//displaying the time 
+//displaying the time and day
 function displayTime(){
-    var timeNow = moment().format('DD MMM YYYY [at] hh:mm:ss a')
-    liveTime.text(timeNow)
+    var timeNow = moment().format('DD MMM YYYY [at] hh:mm:ss a');
+    liveTime.text(timeNow);
 }
-setInterval (displayTime, 1000)
+
+setInterval (displayTime, 1000);
+
+//time blocks
+var workHours = [9, 10 ,11, 12, 13, 14, 15, 16, 17];
+
+function blockTime(){
+    for (var i=0; i < workHours.length; i++){
+        
+     //creating each blocks
+        var blockEL = $("<div>");
+        blockEL.addClass("row time-block");
+        
+        //create first column for the time 
+        var hourlyTimeEL = $("<div>");
+        hourlyTimeEL.addClass("col-1 hour");
+        hourlyTimeEL.text(moment().hour(workHours[i]).format("hA"));
+        blockEL.append(hourlyTimeEL);
+
+        //create second column for the text area
+        var textInputColEL = $("<div>");
+        textInputColEL.addClass("col-10");
+        var inputEL = $("<input>");
+        textInputColEL.append(inputEL);
+
+        //changing the colour of the times in the blocks
+        if (moment().hour() > workHours[i]){
+            textInputColEL.addClass("past");
+        }else if (moment().hour() < workHours[i] ){
+            textInputColEL.addClass("future");
+        }else if (moment().hour()=== workHours[i]){
+            textInputColEL.addClass("present");
+        }
+
+        blockEL.append(textInputColEL);
+        $("#time-block-container").append(blockEL);
+        
+
+        
+    }
+}
+
+
+blockTime();
